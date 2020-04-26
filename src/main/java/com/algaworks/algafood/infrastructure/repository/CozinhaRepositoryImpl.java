41dc2622d8c22307bd4f82e7a8b4c5eddcc7cbff
Bruno Souza <brunoprogramadorjava@gmail.com>
2020-04-25 23:51:32 -0300

    [5.4] Refatorando o codigo para usar o repositorio do Spring Data JPA

--- src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -12,31 +12,25 @@ import org.springframework.transaction.annotation.Transactional;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
-@Component
-public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+public class CozinhaRepositoryImpl {
 
 	@PersistenceContext
 	private EntityManager manager;
 	
-	@Override
 	public List<Cozinha> listar() {
 		return manager.createQuery("from Cozinha", Cozinha.class)
 				.getResultList();
 	}
 	
-	@Override
 	public Cozinha buscar(Long id) {
 		return manager.find(Cozinha.class, id);
 	}
 	
-	@Transactional
-	@Override
 	public Cozinha salvar(Cozinha cozinha) {
 		return manager.merge(cozinha);
 	}
 	
-	@Transactional
-	@Override
 	public void remover(Long id) {
 		Cozinha cozinha = buscar(id);
 		
